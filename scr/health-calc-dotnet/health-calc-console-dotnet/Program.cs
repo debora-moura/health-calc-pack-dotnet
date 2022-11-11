234062681b4cdd99f8dbbd291ee9611e79d054ed
Débora Moura <100494953+debora-moura@users.noreply.github.com>
2022-11-11 00:13:08 -0300

    Arquitetura executavel, cálculo IMC e teste unitários

---
+++ scr/health-calc-dotnet/health-calc-console-dotnet/Program.cs
@@ -0,0 +1,19 @@
+﻿using health_calc_dotnet;
+
+Console.WriteLine("Entre com sua altura e peso para calcular seu IMC!");
+
+Console.WriteLine("Altura: ");
+var Height = Console.ReadLine();
+
+Console.WriteLine("Peso: ");
+var Weight = Console.ReadLine();
+
+IMC objIMC = new IMC();
+
+double Result = objIMC.Calc(double.Parse(Height), double.Parse(Weight));    
+string Category =objIMC.GetIMCCategory(Result);
+
+Console.WriteLine("Oresultado do seu IMC foi: " + Category);
+
+Console.ReadKey();  
+
---
+++ scr/health-calc-dotnet/health-calc-dotnet/Interfaces/IIMC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace health_calc_dotnet.Interfaces
+{
+    public interface IIMC
+    {
+        double Calc(double Height, double Weight);
+        string GetIMCCategory(double IMC);
+
+        bool IsValidData(double Height, double Weight);
+
+
+
+
+    }
+}
